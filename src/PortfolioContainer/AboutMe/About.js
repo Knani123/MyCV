2646c0fd59d5e297791e108f2804d3b6adf45170
Yosri Knani <yosri@cairtu.local>
2022-07-18 13:42:15 +0100

    Add Modif

--- src/PortfolioContainer/AboutMe/About.js
+++ src/PortfolioContainer/AboutMe/About.js
@@ -24,45 +24,51 @@ const About = () => {
         </div>
         <div className="why">
           <Typography variant="body2">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse
-            perspiciatis quo suscipit maxime ad odio necessitatibus accusantium
-            doloribus doloremque a unde eum corporis, aut accusamus laudantium?
-            Voluptate necessitatibus accusantium culpa!
+            Durant 6 ans d'expérience dans le domaine de l'ingénierie j'ai
+            acquis de nombreuses compétences et connaissances qui me permettent
+            de réussir dans les défis journaliers de mon métier . J'ai des
+            compétences solides qui m'ont permis d'acquérir une maîtrise
+            approfondie de la gestion de projet dans la conception et le
+            développement des machines dans les relations et la communication
+            avec les autres et en utilisation des nouveaux technologies .
           </Typography>
           <br />
           <ul>
             <li>
               {" "}
               <Typography variant="subtitle2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Debitis, dicta.
+                Gérer des projets des Qualification et Validation.
               </Typography>
             </li>
             <li>
               {" "}
               <Typography variant="subtitle2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Ratione, et.
-              </Typography>
-            </li>
-            <li>
-              {" "}
-              <Typography variant="subtitle2">Lorem ipsum dolor sit</Typography>
-            </li>
-            <li>
-              {" "}
-              <Typography variant="subtitle2">Lorem ipsum elit.</Typography>
-            </li>
-            <li>
-              {" "}
-              <Typography variant="subtitle2">
-                Lorem dolo consectetur adipisicing elit. Quo, ipsa.
+                Mise en route des nouvelles machine.
               </Typography>
             </li>
             <li>
               {" "}
               <Typography variant="subtitle2">
-                Lorem adipisicing elit. Enim, minima?
+                Lancement, coordination et pilotage des projets
+                d'industrialisation.
+              </Typography>
+            </li>
+            <li>
+              {" "}
+              <Typography variant="subtitle2">
+                Réalisation d’un dossier de qualification.
+              </Typography>
+            </li>
+            <li>
+              {" "}
+              <Typography variant="subtitle2">
+                Developement et conception.
+              </Typography>
+            </li>
+            <li>
+              {" "}
+              <Typography variant="subtitle2">
+                Analyser les resultat et prendre des décisions
               </Typography>
             </li>
           </ul>
